d8603a7ece990a477d396900b792f8dedda776c8
Denis Fernando <denisferoli@gmail.com>
2021-11-09 21:33:06 -0300

    parOuImpar

---
+++ parOuImpar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    /* Algoritmo Par Ou Impar em C */
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    if (numero % 2 == 0)
+    {
+        printf("O numero %d e Par!", numero);
+    }
+    else
+    {
+        printf("O numero %d e Impar", numero);
+    }
+    
+    return 0;
+}
